24ffd3ff4482a99989d1667d6ee8ae662713973f
Nicolò Tognoni <nicolo.tognoni1@gmail.com>
2024-11-11 18:15:37 +0100

    fix popup con spazi nella descrizione

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const NAV_BAR = document.getElementById('navBar');
 const NAV_LIST = document.getElementById('navList');
 const HERO_HEADER = document.getElementById('heroHeader');
 const HAMBURGER_BTN = document.getElementById('hamburgerBtn');
-const NAV_LINKS = Array.from( document.querySelectorAll('.nav__list-link'));
+const NAV_LINKS = Array.from(document.querySelectorAll('.nav__list-link'));
 const SERVICE_BOXES = document.querySelectorAll('.service-card__box');
 const ACTIVE_LINK_CLASS = 'active';
 const BREAKPOINT = 576;
@@ -11,7 +11,7 @@ let currentServiceBG = null;
 let currentActiveLink = document.querySelector('.nav__list-link.active');
 
 // Remove the active state once the breakpoint is reached
-const resetActiveState = ()=>{
+const resetActiveState = () => {
   NAV_LIST.classList.remove('nav--active');
   Object.assign(NAV_LIST.style, {
     height: null
@@ -35,18 +35,18 @@ const addPaddingToHeroHeaderFn = () => {
   });
 }
 addPaddingToHeroHeaderFn();
-window.addEventListener('resize', ()=>{
+window.addEventListener('resize', () => {
   addPaddingToHeroHeaderFn();
 
   // When the navbar is active and the window is being resized, remove the active state once the breakpoint is reached
-  if(window.innerWidth >= BREAKPOINT){
+  if (window.innerWidth >= BREAKPOINT) {
     addPaddingToHeroHeaderFn();
     resetActiveState();
   }
 });
 
 // As the user scrolls, the active link should change based on the section currently displayed on the screen.
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', () => {
   const sections = document.querySelectorAll('#heroHeader, #services, #works, #contact');
 
   // Loop through sections and check if they are visible
@@ -56,7 +56,7 @@ window.addEventListener('scroll', ()=>{
     if (window.scrollY >= sectionTop - NAV_BAR_HEIGHT) {
       const ID = section.getAttribute('id');
       const LINK = NAV_LINKS.filter(link => {
-        return link.href.includes('#'+ID);
+        return link.href.includes('#' + ID);
       })[0];
       console.log(LINK);
       currentActiveLink.classList.remove(ACTIVE_LINK_CLASS);
@@ -67,7 +67,7 @@ window.addEventListener('scroll', ()=>{
 });
 
 // Shows & hide navbar on smaller screen
-HAMBURGER_BTN.addEventListener('click', ()=>{
+HAMBURGER_BTN.addEventListener('click', () => {
   NAV_LIST.classList.toggle('nav--active');
   if (NAV_LIST.classList.contains('nav--active')) {
     Object.assign(document.body.style, {
@@ -88,7 +88,7 @@ HAMBURGER_BTN.addEventListener('click', ()=>{
 
 // When navbar link is clicked, reset the active state
 NAV_LINKS.forEach(link => {
-  link.addEventListener('click', ()=>{
+  link.addEventListener('click', () => {
     resetActiveState();
     link.blur();
   })
@@ -145,19 +145,20 @@ const modalExtraInfo = document.getElementById('modalExtraInfo'); // New extra i
 
 // Define project details
 const portfolioProjects = {
-    project1: {
-        image: "./assets/images/popup/aerial_popup.jpg",
-        title: "Satellite Image Classification with a CNN",
-        description: `
+  project1: {
+    image: "./assets/images/popup/aerial_popup.jpg",
+    title: "Satellite Image Classification with a CNN",
+    description: `
             <p>Using a Convolutional Neural Network to classify satellite images into different categories.</p>
-
+            <br>
             <p>The code is entirely written in Python using TensorFlow. I used Google Colab and stored the images in Google Drive.</p>
-
+            <br>
             <p><strong>Image Loading and Augmentation</strong><br>
             To load the images, I used ImageDataGenerators and applied Data Augmentation to reduce overfitting and improve the accuracy of the model.</p>
-
+            <br>
             <p><strong>Model Architecture</strong><br>
             The CNN used is composed of the following layers:</p>
+            <br>
             <ul>
                 <li>Conv2D with 8 filters and a kernel of 3x3</li>
                 <li>ReLU</li>
@@ -172,93 +173,97 @@ const portfolioProjects = {
                 <li>Dense of 300 units with Sigmoid function</li>
                 <li>Dense of 21 units with Softmax function</li>
             </ul>
+            <br>
             <p>This model reached an accuracy of 82%.</p>
-
+            <br>
             <p><strong>Transfer Learning with InceptionV3</strong><br>
             In the final part of the code, I implemented Transfer Learning using the pre-trained InceptionV3 on ImageNet from Google, achieving an accuracy of 93% on the validation set.</p>
-
+            <br>
             <p><strong>How to Run the Notebook</strong><br>
             We pre-ran the entire notebook and kept the results of the different models. However, to run it yourself, you’ll need to follow these steps:</p>
+            <br>
             <ul>
                 <li>Upload the folder with the images in your Google Drive under the path: drive/MyDrive/Colab Notebooks/UCMerced_LandUse</li>
                 <li>Each section of code is organized into 'Chapters', allowing for independent execution.</li>
             </ul>
-
+            <br>
             <p><strong>Dataset</strong><br>
+            <br>
             I used the UC Merced Land Use Dataset, composed of 21 classes, each of 256x256 pixels.</p>
         `,
-        badges: ["Python", "TensorFlow"],
-        link: "https://github.com/nicolotognoni/Satellite_Image_Classification_CNN"
-    },
-    project2: {
-      image: "./assets/images/popup/stylegan_popup.jpg",
-        title: "GAN for Increasing CNN Accuracy",
-        description: `
+    badges: ["Python", "TensorFlow"],
+    link: "https://github.com/nicolotognoni/Satellite_Image_Classification_CNN"
+  },
+  project2: {
+    image: "./assets/images/popup/stylegan_popup.jpg",
+    title: "GAN for Increasing CNN Accuracy",
+    description: `
             <p>This project is part of my Master’s thesis, titled: "Using data generated by a GAN to improve the accuracy of a CNN image classification model in the case of a small amount of initial data".</p>
-
+            <br>    
             <p><strong>Abstract</strong><br>
             In certain real-world situations, it’s either impossible or too expensive to gather a huge quantity of data to effectively train deep learning models. The objective of the thesis was to overcome this challenge by using additional data artificially generated by GAN models.</p>
-
+            <br>
             <p>We aimed to see if the additional data created using the initial data would improve the performance of the model.</p>
-
+            <br>
             <p>We expect a threshold in the amount of initial data below which using GAN-generated synthetic data would not be effective. The goal was to find the minimum data needed to improve the CNN model with the help of GANs.</p>
-
+            <br>
             <p><strong>Info</strong></p>
+            <br>
             <ul>
                 <li><strong>StyleGAN2_ADA.ipynb</strong> - Code to train the StyleGAN model</li>
                 <li><strong>generate_synthetic_data.ipynb</strong> - Code to generate synthetic images from a pre-trained StyleGAN model</li>
                 <li><strong>CNN.ipynb</strong> - Convolutional Neural Network code to test accuracy before and after adding synthetic images</li>
             </ul>
         `,
-        
-        badges: ["Python", "StyleGAN2_ADA"],
-        link: "https://github.com/nicolotognoni/GAN_for_increasing_CNN_accuracy"
-    },
-    project3: {
-        image: "./assets/works/sample.png", // Added image for consistency
-        title: "My Landing Page",
-        description: "Created a landing page using HTML, CSS, and JavaScript to showcase web development skills and user interface design.",
-        badges: ["HTML", "CSS", "JavaScript"],
-        link: ""
-    }
+
+    badges: ["Python", "StyleGAN2_ADA"],
+    link: "https://github.com/nicolotognoni/GAN_for_increasing_CNN_accuracy"
+  },
+  project3: {
+    image: "./assets/works/sample.png", // Added image for consistency
+    title: "My Landing Page",
+    description: "Created a landing page using HTML, CSS, and JavaScript to showcase web development skills and user interface design.",
+    badges: ["HTML", "CSS", "JavaScript"],
+    link: ""
+  }
 };
 
 function openPortfolioModal(projectId) {
-    const project = portfolioProjects[projectId];
-    modalTitle.textContent = project.title;
-    modalDescription.textContent = project.description;
-    repositoryLink.href = project.link; // Set link for repository button
-    modalImage.src = project.image || '';  // Set image
+  const project = portfolioProjects[projectId];
+  modalTitle.textContent = project.title;
+  modalDescription.innerHTML = project.description;
+  repositoryLink.href = project.link; // Set link for repository button
+  modalImage.src = project.image || '';  // Set image
 
-    // Clear existing badges and add new ones
-    modalBadges.innerHTML = '';
-    project.badges.forEach(badge => {
-        const badgeElem = document.createElement('span');
-        badgeElem.className = 'work__badge';
-        badgeElem.textContent = badge;
-        modalBadges.appendChild(badgeElem);
-    });
+  // Clear existing badges and add new ones
+  modalBadges.innerHTML = '';
+  project.badges.forEach(badge => {
+    const badgeElem = document.createElement('span');
+    badgeElem.className = 'work__badge';
+    badgeElem.textContent = badge;
+    modalBadges.appendChild(badgeElem);
+  });
 
-    portfolioModal.style.display = 'flex';
+  portfolioModal.style.display = 'flex';
 }
 
 
 // Close modal on clicking the close button
 closePortfolioModalButton.onclick = function () {
-    portfolioModal.style.display = 'none';
+  portfolioModal.style.display = 'none';
 }
 
 // Close modal when clicking outside of the modal content
 window.onclick = function (event) {
-    if (event.target == portfolioModal) {
-        portfolioModal.style.display = 'none';
-    }
+  if (event.target == portfolioModal) {
+    portfolioModal.style.display = 'none';
+  }
 }
 
 // Add event listeners to each project box
 document.querySelectorAll('.work').forEach((projectBox) => {
-    projectBox.addEventListener('click', () => {
-        const projectId = projectBox.getAttribute('data-project-id');
-        openPortfolioModal(projectId);
-    });
+  projectBox.addEventListener('click', () => {
+    const projectId = projectBox.getAttribute('data-project-id');
+    openPortfolioModal(projectId);
+  });
 });
